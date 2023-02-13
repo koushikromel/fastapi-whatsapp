29952ad81b232110db7703446d6ce30f7ead14e8
Koushik Romel <koushikromel@gmail.com>
2023-02-13 17:01:28 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,33 @@
+from flask import Flask, request
+from fastapi.templating import Jinja2Templates
+import requests
+
+app = Flask(__name__)
+templates = Jinja2Templates(directory="templates")
+
+account_sid = 'AC97b9370cc504c56bf6c4c7590ba7d533'
+auth_token = '43db9a42bd10eb8ea60a29fd5c1088d8'
+
+
+@app.route("/")
+def home():
+    base_url = "https://api.twilio.com/2010-04-01/Accounts/"
+    messages_url = f"{base_url}{account_sid}/Messages.json"
+    response = requests.get(messages_url, auth=(account_sid, auth_token))
+    if response.status_code == 200:
+        messages = response.json()["messages"]
+        return templates.TemplateResponse("messages.html", {"request":request, "messages":messages})
+    else:
+        return {"error": response.text}
+
+
+@app.route("/webhook", methods=["POST"])
+def webhook():
+    message = request.values.get('Body', '')
+    sender = request.values.get('From', '')
+    print(sender, " sent ", message)
+    return "Succeess"
+
+
+if __name__ == "__main__":
+    app.run(host="139.144.4.238", debug=True, port=8888)
